871a39017aded067d408213882ec297a08e191d5
spolisetty <spolisetty@osius.com>
2019-05-27 15:31:53 +0530

    adding java programs

---
+++ JavaPrograms/src/javaInterviewPrograms/dummy.java
@@ -0,0 +1,25 @@
+package javaInterviewPrograms;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class dummy {
+	
+
+	public static void main(String[] args) {
+		
+		String s="SATISH KUMAR POLISETTY";
+				
+		String s1=s.substring(12);
+		String s2=s.substring(0, 6);
+		String s3=s.substring(6, 12);
+		System.out.println(s1+ " "+s2+ ""+s3);
+	}
+       
+		
+		
+		
+		
+		
+	}
